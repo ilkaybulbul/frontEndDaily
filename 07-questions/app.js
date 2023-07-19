737763f18936662501ea687cd2f44a394923dc6c
ilkay bülbül <75014140+ilkaybulbul@users.noreply.github.com>
2023-07-19 20:59:19 +0300

    Add files via upload

---
+++ 07-questions/app.js
@@ -0,0 +1,29 @@
+//using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question){
+    const btn = question.querySelector('.question-btn')
+
+    btn.addEventListener('click', function(){
+
+       questions.forEach(function (item){
+        if(item !== question){
+            item.classList.remove('show-text')
+        }
+       })
+
+        question.classList.toggle('show-text');
+    })
+})
+
+
+// first solituon
+// traversing the dom
+// const btn  = document.querySelectorAll('.question-btn')
+
+// btn.forEach(function(btn) {
+//     btn.addEventListener('click', function(e){
+//        const question =  e.currentTarget.parentElement.parentElement;
+//        question.classList.toggle('show-text');
+//     })
+// })
